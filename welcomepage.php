bfbee4183a33537e391a5c1b387693fb30bb438d
17-34858-2 <17-34858-2@student.aiub.edu>
2021-06-21 23:18:09 +0600

    First Commit

---
+++ welcomepage.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome Page</title>
+</head>
+<body>
+	<h2> Log In Successfull. </h2>
+	<h1><span style="color:green;">Welcome to our website!!</span></h1>
+
+</body>
+</html>
